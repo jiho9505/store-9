37ffbb6ec9e7b7eb220b1ef692ee090094339202
jihoMoon <sun_days@naver.com>
2021-08-10 11:32:41 +0900

    🔧  Prittier 세팅

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+/**
+ * semi             : Line 끝에 semicolon을 넣도록 합니다.
+ * bracketSpacing   : {foo:bar} => { foo: bar }
+ * arrowParens      : 항상 arrow function의 인자는 괄호로 둘러쌓여 있습니다.
+ * singleQuotes     : string 은 singleQuote를 사용합니다.
+ */
+
+module.exports = {
+  semi: true,
+  tabWidth: 2,
+  printWidth: 100,
+  arrowParens: 'always',
+  singleQuote: true,
+};
