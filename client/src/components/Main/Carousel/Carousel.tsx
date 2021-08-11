70234f535f257335a7df36989ab57eab24e277a6
jihoMoon <sun_days@naver.com>
2021-08-11 18:59:48 +0900

    ✨ Add Carousel UI

---
+++ client/src/components/Main/Carousel/Carousel.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
+import '@/static/assets/img/bannerImage1.gif';
+import '@/static/assets/img/bannerImage2.gif';
+import '@/static/assets/img/bannerImage3.gif';
+
+const Carousel = () => {
+  const [index, setIndex] = useState(1);
+  const [clickEventActive, setClickEventActive] = useState(false);
+
+  !clickEventActive &&
+    setTimeout(() => {
+      const standardIndex = index + 1 > 3 ? 1 : index + 1;
+      setIndex(standardIndex);
+    }, 3000);
+
+  return (
+    <CarouselContainer>
+      <ImageContainer translateX={index}>
+        <SlideImage src="images/bannerImage1.gif" />
+        <SlideImage src="images/bannerImage2.gif" />
+        <SlideImage src="images/bannerImage3.gif" />
+      </ImageContainer>
+      <ButtonContainer>
+        <Button onClick={onClickButton} data-idx="1" active={index === 1 ? true : false}></Button>
+        <Button onClick={onClickButton} data-idx="2" active={index === 2 ? true : false}></Button>
+        <Button onClick={onClickButton} data-idx="3" active={index === 3 ? true : false}></Button>
+      </ButtonContainer>
+    </CarouselContainer>
+  );
+};
+
+type ImageProps = {
+  translateX: number;
+};
+
+type ButtonProps = {
+  active: boolean;
+};
+
+const CarouselContainer = styled.div`
+  position: relative;
+`;
+
+const ImageContainer = styled.div<ImageProps>`
+  display: flex;
+  width: 100%;
+  transition: all 0.5s ease-in-out;
+  transform: translateX(${(props) => `${(props.translateX - 1) * -100}%`});
+`;
+
+const SlideImage = styled.img`
+  width: 100%;
+  cursor: pointer;
+  opacity: 1;
+`;
+
+const ButtonContainer = styled.div`
+  position: absolute;
+  left: 0px;
+  bottom: 10px;
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  z-index: 1;
+  gap: 20px;
+`;
+
+const Button = styled.button<ButtonProps>`
+  width: 8px;
+  height: 8px;
+  border-radius: 100%;
+  background: ${(props) => (props.active ? 'white' : '#b5b5b5')};
+`;
+
+export default Carousel;
