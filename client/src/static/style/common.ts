11a22c2bd340fae3a6a4841d5a96a53b46756820
jihoMoon <sun_days@naver.com>
2021-08-11 19:11:22 +0900

    💄 Add button color

--- client/src/static/style/common.ts
+++ client/src/static/style/common.ts
@@ -2,11 +2,9 @@ type Color = string;
 type Font = string;
 
 export const primary1: Color = '#2AC1BC';
-
 export const greySpan: Color = '#717171';
-
 export const greyBg1: Color = '#f6f6f6';
-
 export const greyLine: Color = '#CCD3D3';
+export const greyButton: Color = '#b5b5b5';
 
 export const baeminFont: Font = `'Do Hyeon', sans-serif`;
