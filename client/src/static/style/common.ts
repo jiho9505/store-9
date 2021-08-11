e09a974a2c6e912ba40fded33fc6fe78fa81eac7
LeeJaeyun <ag502@naver.com>
2021-08-11 15:24:42 +0900

    💄 container width global style로 분리

--- client/src/static/style/common.ts
+++ client/src/static/style/common.ts
@@ -1,5 +1,6 @@
 type Color = string;
 type Font = string;
+type Size = string;
 
 export const primary1: Color = '#2AC1BC';
 
@@ -10,3 +11,5 @@ export const greyBg1: Color = '#f6f6f6';
 export const greyLine: Color = '#CCD3D3';
 
 export const baeminFont: Font = `'Do Hyeon', sans-serif`;
+
+export const normalContainerWidth: Size = '1200px';
