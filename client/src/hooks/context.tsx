9e456035e56a247a98f848a20250cfadd25b8f75
LeeJaeyun <ag502@naver.com>
2021-08-11 17:33:54 +0900

    🚚 history context 분리

---
+++ client/src/hooks/context.tsx
@@ -0,0 +1,9 @@
+import React, { createContext } from 'react';
+
+type historyContextType = {
+  curLocation: string;
+  onChangeLocation?: (path: string) => void;
+  history?: History;
+};
+
+export const HistoryContext = createContext<historyContextType>({ curLocation: '/' });
