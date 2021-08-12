0d619fee4aa1bc64b2e1cd8912b37f033b0de009
LeeJaeyun <ag502@naver.com>
2021-08-12 11:12:25 +0900

    ✨ price pannel 디자인 수정

--- client/src/components/base/PricePannel/index.tsx
+++ client/src/components/base/PricePannel/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled';
 
-import { normalContainerWidth, greyLine } from '@/static/style/common';
+import { normalButtonRadius, primary1, white } from '@/static/style/common';
 
 type PricePannelProps = {
   totalProductNumber: number;
@@ -13,7 +13,7 @@ const PricePannel = ({ totalProductNumber, productTotalPrice }: PricePannelProps
   return (
     <PricePannelContainer>
       <ProductTotalPrice>
-        <span>총 {totalProductNumber}개의 상품금액</span>
+        <span>상품금액</span>
         <span>{productTotalPrice.toLocaleString()}원</span>
       </ProductTotalPrice>
       <DeliveryTotalPrice>
@@ -24,36 +24,41 @@ const PricePannel = ({ totalProductNumber, productTotalPrice }: PricePannelProps
         <span>합계</span>
         <span>{(productTotalPrice + deliveryCost).toLocaleString()}원</span>
       </TotalPrice>
+      <OrderButton>주문하기</OrderButton>
     </PricePannelContainer>
   );
 };
 
 const PricePannelContainer = styled.div`
   display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  width: ${normalContainerWidth};
-  height: 130px;
-  border: 2px solid ${greyLine};
+  flex-direction: column;
+  justify-content: space-between;
+  width: 400px;
   padding: 20px;
 `;
 
 const ProductTotalPrice = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: flex-end;
+  justify-content: space-between;
 `;
 
 const DeliveryTotalPrice = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: flex-end;
+  justify-content: space-between;
 `;
 
 const TotalPrice = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: flex-end;
+  justify-content: space-between;
+`;
+
+const OrderButton = styled.button`
+  background-color: ${primary1};
+  color: ${white};
+  margin-top: 15px;
+  width: 100%;
+  height: 40px;
+  border-radius: ${normalButtonRadius};
 `;
 
 export default PricePannel;
