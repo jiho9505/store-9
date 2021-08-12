bd1e90a7b381d8a07057322402d8adebaf456cea
jihoMoon <sun_days@naver.com>
2021-08-12 14:28:43 +0900

    ✨  Add Colors in common.ts

--- client/src/static/style/common.ts
+++ client/src/static/style/common.ts
@@ -6,5 +6,9 @@ export const greySpan: Color = '#717171';
 export const greyBg1: Color = '#f6f6f6';
 export const greyLine: Color = '#CCD3D3';
 export const greyButton: Color = '#b5b5b5';
+export const baemin: Color = '#4bc2cb';
+export const green: Color = '#3e8d28';
+export const red1: Color = '#ed2e3f';
+export const red2: Color = '#ff6350';
 
 export const baeminFont: Font = `'Do Hyeon', sans-serif`;
