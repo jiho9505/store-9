d121ca50732f362619908441f6ad61c2b4ce75c9
junbeoman <wnsqja8744@gmail.com>
2021-08-12 13:12:15 +0900

    🎨 remove validation logic

--- client/src/hooks/customHooks/useInput.ts
+++ client/src/hooks/customHooks/useInput.ts
@@ -1,43 +1,21 @@
 import React, { useState, useCallback } from 'react';
 
-const INPUT_ERROR_MESSAGE = (name) => `${name} 입력값이 잘못되었습니다. 다시 한번 확인해주세요.`;
+const useInput = <TFields extends {}>(initialState: TFields) => {
+  const [form, setForm] = useState(initialState);
+  const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = e.target;
 
-const useInput = <TFields extends {}>(
-  initialState: TFields,
-  validator?: (value: string) => boolean
-): [TFields, (e: React.ChangeEvent<HTMLInputElement>) => void, () => void, string] => {
-  const [inputValue, setInput] = useState(initialState);
-  const [error, setError] = useState('');
-
-  const onChange = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      const { value, name } = e.target;
-      let isValidValue = true;
-      if (validator !== undefined) {
-        isValidValue = validator(value);
-      }
-
-      if (isValidValue) {
-        setInput((prevForm) => ({
-          ...prevForm,
-          [name]: value,
-        }));
-        if (error) {
-          setError('');
-        }
-      } else {
-        setError(INPUT_ERROR_MESSAGE(name));
-      }
-    },
-    [validator]
-  );
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: value,
+    }));
+  }, []);
 
   const reset = useCallback(() => {
-    setInput(initialState);
-    setError('');
+    setForm(initialState);
   }, [initialState]);
 
-  return [inputValue, onChange, reset, error];
+  return { form, onChange, reset };
 };
 
 export default useInput;
