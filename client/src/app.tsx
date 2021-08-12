417b3599ecee2708eaeab377e921fbfa7cc63221
jihoMoon <sun_days@naver.com>
2021-08-12 22:38:05 +0900

    ✨  Add ProductList Component in app

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -3,10 +3,15 @@ import { Router, Route, Link } from './Router';
 
 import Header from '@/components/base/Header';
 import Footer from '@/components/base/Footer/Footer';
-import Main from '@/pages/Main/Main';
+import Main from '@/pages/Main';
+import ProductList from '@/pages/ProductList';
 
 import '@/static/assets/img/baeminFavicon.png';
 
+/**
+ * FIXME:
+ * 추후 여러 경로가 동일 컴포넌트 가리키는 것에 대한 처리 필요
+ */
 const App = () => {
   return (
     <>
@@ -20,6 +25,36 @@ const App = () => {
         </Route>
         <Route exact path="/product/:id">
           <ProductPage />
+        </Route>
+        <Route exact path="/total">
+          <ProductList />
+        </Route>
+        <Route exact path="/suplies">
+          <ProductList />
+        </Route>
+        <Route exact path="/living">
+          <ProductList />
+        </Route>
+        <Route exact path="/books">
+          <ProductList />
+        </Route>
+        <Route exact path="/green">
+          <ProductList />
+        </Route>
+        <Route exact path="/smile-edition">
+          <ProductList />
+        </Route>
+        <Route exact path="/euljiro-edition">
+          <ProductList />
+        </Route>
+        <Route exact path="/baedal-friends">
+          <ProductList />
+        </Route>
+        <Route exact path="/present">
+          <ProductList />
+        </Route>
+        <Route exact path="/collaborate">
+          <ProductList />
         </Route>
         <Navigation />
       </Router>
@@ -46,3 +81,16 @@ const Navigation = () => {
 };
 
 export default App;
+
+// import Suplies from '@/pages/ProductList/Suplies';
+// import Living from '@/pages/ProductList/Living';
+// import Books from '@/pages/ProductList/Books';
+// import Green from '@/pages/ProductList/Green';
+// import SmileEdition from '@/pages/ProductList/SmileEdition';
+// import EuljiroEdition from '@/pages/ProductList/EuljiroEdition';
+// import BaedalFriends from '@/pages/ProductList/BaedalFriends';
+// import Present from '@/pages/ProductList/Present';
+// import Collaborate from '@/pages/ProductList/Collaborate';
+{
+  /* */
+}
