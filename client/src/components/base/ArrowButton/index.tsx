aec992770fecd2973146cadbdd154c8a4b918dc8
jihoMoon <sun_days@naver.com>
2021-08-13 18:26:13 +0900

    ✨  Complete ArrowButton

--- client/src/components/base/ArrowButton/index.tsx
+++ client/src/components/base/ArrowButton/index.tsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import { baemin } from '@/static/style/common';
 
 const ArrowButton = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const standardHeight = 800;
+
+  document.addEventListener('scroll', () => {
+    if (isActive && window.scrollY < standardHeight) setIsActive(false);
+    else if (!isActive && window.scrollY > standardHeight) setIsActive(true);
+  });
+
+  const onClickHandler = (e: React.MouseEvent) => {
+    const { target } = e;
+    if (!(target instanceof HTMLElement)) return;
+
+    if (target.closest('.arrow-button')) {
+      const scrollTo = document.querySelector('.header');
+      scrollTo.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
-    <Button>
+    <Button className="arrow-button" onClick={onClickHandler} active={isActive}>
       <i className="fas fa-arrow-up"></i>
     </Button>
   );
 };
 
-const Button = styled.button`
+type ButtonProps = {
+  active: boolean;
+};
+
+const Button = styled.button<ButtonProps>`
   position: fixed;
   bottom: 50px;
   right: 300px;
@@ -20,14 +43,10 @@ const Button = styled.button`
   background-color: ${baemin};
   color: white;
   border-radius: 10px;
-  opacity: 0;
-  pointer-events: none;
+  opacity: ${(props) => (props.active ? 1 : 0)};
+  pointer-events: ${(props) => (props.active ? 'auto' : 'none')};
   transition: all 0.5s ease-in;
   box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.3);
 `;
 
-// .arrow-up.visible {
-//     opacity: 1;
-//     pointer-events: auto;
-// }
 export default ArrowButton;
