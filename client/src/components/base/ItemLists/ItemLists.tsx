7e0ee3e4460e1520708c4ddd60038a3d5d2d1ff7
jihoMoon <sun_days@naver.com>
2021-08-16 16:03:04 +0900

    ✨  Adjust InterSectionObserverAPI

--- client/src/components/base/ItemLists/ItemLists.tsx
+++ client/src/components/base/ItemLists/ItemLists.tsx
@@ -8,39 +8,29 @@ import ItemContent from './ItemContent/ItemContent';
 import '@/static/assets/img/sampleItem.jpeg';
 import '@/static/assets/img/soldout.png';
 
-/**
- * TODO:
- * 현재는 테스트용 더미데이터이므로
- * array, feature, getColor 는 추후 데이터가 확정되면
- * 바꿀 예정
- */
-const array = [
-  { discount: true, quantity: 0 },
-  { discount: false, quantity: 1 },
-  { discount: true, quantity: 0 },
-  { discount: false, quantity: 1 },
-  { discount: true, quantity: 1 },
-  { discount: false, quantity: 0 },
-];
-
 type ItemListsProps = {
   observeTag?: () => void;
+  products;
 };
 
-const ItemLists = ({ observeTag }: ItemListsProps) => {
+/**
+ * TODO:
+ * ItemImage에 src만 주면 될듯
+ */
+const ItemLists = ({ observeTag, products }: ItemListsProps) => {
   const createItem = () => {
-    return array.map((item, idx) => (
+    return products.map((item, idx) => (
       <Item key={idx} className="item">
         <ItemImage quantity={item.quantity}></ItemImage>
-        <ItemContent quantity={item.quantity} discount={item.discount}></ItemContent>
-        {item.quantity ? <ItemLabel quantity={item.quantity}></ItemLabel> : ``}
+        <ItemContent item={item}></ItemContent>
+        {item.quantity ? <ItemLabel product={item}></ItemLabel> : ``}
       </Item>
     ));
   };
 
   useEffect(() => {
     observeTag && observeTag();
-  }, []);
+  }, [products]);
 
   return (
     <>
