062522fdee14057bca2e4c821936ca678418202c
LeeJaeyun <ag502@naver.com>
2021-08-17 21:48:16 +0900

    ✨ MyPage 헤더 구현

--- client/src/pages/MyPage/index.tsx
+++ client/src/pages/MyPage/index.tsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import guguStyled from '@/core/styled';
+
+import { Route } from '@/Router';
+import { LeftNav } from '@/components/MyPage';
+
+import { normalContainerWidth, baeminThickFont } from '@/static/style/common';
 
 const MyPage = () => {
-  return <div>This is My Page</div>;
+  return (
+    <MyPageContainer>
+      <LeftNav />
+      <MyPageContentContiner>
+        <MyPageHeader>반가워요</MyPageHeader>
+        <UserName>OOO 님</UserName>
+        <Route path="/mypage/like">
+          <div>TEST</div>
+        </Route>
+      </MyPageContentContiner>
+    </MyPageContainer>
+  );
 };
+
+const MyPageContainer = guguStyled.div`
+  display: flex;
+  width: ${normalContainerWidth};
+  margin: 0 auto;
+  padding: 40px 0 0 0;
+`;
+
+const MyPageContentContiner = guguStyled.div``;
+
+const MyPageHeader = guguStyled.h2`
+  font-family: ${baeminThickFont};
+  font-size: 40px;
+  margin-bottom: 10px;
+`;
+
+const UserName = guguStyled.span`
+  font-size: 20px;
+`;
 
 export default MyPage;
