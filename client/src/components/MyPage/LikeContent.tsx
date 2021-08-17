6caf012ddb672a92fc5e6b91bc75e7ce8b51a4e1
LeeJaeyun <ag502@naver.com>
2021-08-18 00:34:09 +0900

    ✨ 찜목록 리스트 UI 구현

---
+++ client/src/components/MyPage/LikeContent.tsx
@@ -0,0 +1,44 @@
+import React, { useMemo } from 'react';
+
+import ListTable from '../base/ListTable';
+import TableItem from '../base/TableItem';
+
+// api 확정되면 type 지정
+type LikeContentProps = {
+  likeProducts: any;
+  selectedProduct: Set<number>;
+  onCheck(id: number): void;
+  onCheckAll(e): void;
+};
+
+const tableHeader = [
+  { id: 'productName', name: '상품명/옵션', width: '70%' },
+  { id: 'price', name: '상품금액/수량', width: '10%' },
+  { id: 'total', name: '합계', width: '10%' },
+];
+
+const LikeContent = ({ likeProducts, onCheck, onCheckAll, selectedProduct }: LikeContentProps) => {
+  const tableBody = useMemo(() => {
+    return likeProducts.map((likeProduct) => {
+      return {
+        id: likeProduct.productId,
+        cells: [{ c: <TableItem product={likeProduct} />, colSpan: 3 }],
+      };
+    });
+  }, [likeProducts]);
+
+  return (
+    <div>
+      <ListTable
+        checkable
+        header={tableHeader}
+        body={tableBody}
+        onCheck={onCheck}
+        onCheckAll={onCheckAll}
+        selectedItems={selectedProduct}
+      />
+    </div>
+  );
+};
+
+export default LikeContent;
--- client/src/components/MyPage/index.tsx
+++ client/src/components/MyPage/index.tsx
@@ -1,1 +1,2 @@
 export { default as LeftNav } from './LeftNav';
+export { default as LikeContent } from './LikeContent';
