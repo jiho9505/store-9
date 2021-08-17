d1b68d513ff13868bb2c1824f6f5ede73377c111
jihoMoon <sun_days@naver.com>
2021-08-17 19:21:28 +0900

    ✨  Add OverViewContent Component

---
+++ client/src/components/DetailProduct/OverViewContent/index.tsx
@@ -0,0 +1,184 @@
+import React, { useState } from 'react';
+import { calculateDiscount } from '@/util/calculateDiscount';
+import StockSelector from '../StockSelector';
+import TotalPrice from '../TotalPrice';
+import Like from '../Like';
+import Cart from '../Cart';
+import Buy from '../Buy';
+
+import styled from '@emotion/styled';
+import {
+  baemin,
+  baeminFont,
+  baeminThickFont,
+  greySpan,
+  lightBlack,
+  greyLine,
+} from '@/static/style/common';
+
+type OverViewContentProps = {
+  info;
+};
+
+const OverViewContent = ({ info }: OverViewContentProps) => {
+  const [selectedStock, setSelectedStock] = useState(1);
+  const discountPrice = info.discount_rate ? calculateDiscount(info.price, info.discount_rate) : '';
+
+  const createSellPrice = (price, quantity) => {
+    return (
+      <SellPrice>
+        <PriceText>판매가격</PriceText>
+        <SellPriceValue soldout={!quantity}>{quantity ? `${price}원` : '품절'}</SellPriceValue>
+      </SellPrice>
+    );
+  };
+
+  const createPrice = () => {
+    const discount = info.discount_rate;
+    const price = info.price;
+    const quantity = info.quantity;
+
+    return discount ? (
+      <PriceContainer>
+        {quantity ? (
+          <OriginalPrice>
+            <PriceText>정가</PriceText>
+            <OriginalPriceValue>{price}원</OriginalPriceValue>
+          </OriginalPrice>
+        ) : (
+          ''
+        )}
+        {createSellPrice(discountPrice, quantity)}
+      </PriceContainer>
+    ) : (
+      <PriceContainer> {createSellPrice(price, quantity)}</PriceContainer>
+    );
+  };
+
+  const refreshStock = (value) => {
+    setSelectedStock(value);
+  };
+
+  const makePriceWithoutComma = () => {
+    const price = info.discount_rate ? discountPrice : info.price;
+    const purePrice = price ? Number(price.replace(/[,]/g, '')) : 0;
+    return purePrice;
+  };
+
+  return (
+    <OverviewContent>
+      <Title>{info.title}</Title>
+      {createPrice()}
+      <ShipInfo>
+        <ShipInfoText>배송정보</ShipInfoText>
+        <ShipInfoDetail>
+          <div>2,500원 (3만원 이상 구매시 무료)</div>
+          <div>오후 2시 당일배송마감</div>
+        </ShipInfoDetail>
+      </ShipInfo>
+      <StockSelector
+        title={info.title}
+        price={makePriceWithoutComma()}
+        refreshStock={refreshStock}
+        selectedStock={selectedStock}
+        currStock={info.quantity}
+      ></StockSelector>
+      <ContentBottomContainer>
+        {info.quantity ? (
+          <>
+            <TotalPrice price={makePriceWithoutComma()} selectedStock={selectedStock}></TotalPrice>
+            <ButtonContainer>
+              <Like />
+              <Cart />
+              <Buy />
+            </ButtonContainer>
+          </>
+        ) : (
+          ''
+        )}
+      </ContentBottomContainer>
+    </OverviewContent>
+  );
+};
+
+export default OverViewContent;
+
+const ButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  gap: 15px;
+  margin-top: 23px;
+`;
+const ContentBottomContainer = styled.div`
+  position: absolute;
+  bottom: 0px;
+  left: 0px;
+  border-top: 1px solid ${greyLine};
+  width: 100%;
+`;
+const OverviewContent = styled.div`
+  width: 556px;
+  height: 585px;
+  position: relative;
+`;
+
+const Title = styled.h2`
+  font-size: 35px;
+  color: ${lightBlack};
+  font-family: ${baeminFont};
+`;
+
+const OriginalPrice = styled.div`
+  display: flex;
+`;
+
+const SellPrice = styled.div`
+  display: flex;
+  margin-top: 25px;
+`;
+
+const PriceContainer = styled.div`
+  margin-top: 40px;
+`;
+
+const PriceText = styled.div`
+  color: ${greySpan};
+  font-family: ${baeminFont};
+  font-size: 19px;
+  width: 100px;
+`;
+
+const OriginalPriceValue = styled.span`
+  text-decoration: line-through;
+  color: ${lightBlack};
+  font-size: 19px;
+`;
+
+type SellPriceValueProps = {
+  soldout: boolean;
+};
+
+const SellPriceValue = styled.span<SellPriceValueProps>`
+  font-family: ${baeminThickFont};
+  color: ${(props) => (props.soldout ? baemin : lightBlack)};
+  font-size: 19px;
+`;
+
+const ShipInfo = styled.div`
+  display: flex;
+  margin-top: 25px;
+`;
+
+const ShipInfoText = styled.div`
+  color: ${greySpan};
+  font-family: ${baeminFont};
+  font-size: 19px;
+  width: 100px;
+`;
+
+const ShipInfoDetail = styled.div`
+  font-family: ${baeminFont};
+  font-size: 17px;
+  color: ${lightBlack};
+`;
