f321e064fd324a5388b65955be9f269a535371ea
jihoMoon <sun_days@naver.com>
2021-08-19 22:13:57 +0900

    💄  Add Star & Like

--- client/src/components/base/ItemLists/ItemLists.tsx
+++ client/src/components/base/ItemLists/ItemLists.tsx
@@ -4,9 +4,11 @@ import styled from '@emotion/styled';
 import ItemImage from './ItemImage/ItemImage';
 import ItemLabel from './ItemLabel/ItemLabel';
 import ItemContent from './ItemContent/ItemContent';
+import StarComponent from '../Star';
 
 import '@/static/assets/img/sampleItem.jpeg';
 import '@/static/assets/img/soldout.png';
+import { red1 } from '@/static/style/common';
 
 type ItemListsProps = {
   observeTag?: () => void;
@@ -15,7 +17,7 @@ type ItemListsProps = {
 
 /**
  * TODO:
- * ItemImage에 src만 주면 될듯
+ * API 연동 후 값 동적으로 넣어야 합니다.
  */
 const ItemLists = ({ observeTag, products }: ItemListsProps) => {
   const createItem = () => {
@@ -24,6 +26,13 @@ const ItemLists = ({ observeTag, products }: ItemListsProps) => {
         <ItemImage productImage={item.image} quantity={item.quantity}></ItemImage>
         <ItemContent item={item}></ItemContent>
         {item.quantity ? <ItemLabel product={item}></ItemLabel> : ``}
+        <StarContainer>
+          <StarComponent score={5} />
+        </StarContainer>
+        <LikeContainer>
+          <i className="fas fa-heart"></i>
+          <span>777</span>
+        </LikeContainer>
       </Item>
     ));
   };
@@ -41,6 +50,23 @@ const ItemLists = ({ observeTag, products }: ItemListsProps) => {
     </>
   );
 };
+
+const LikeContainer = styled.div`
+  margin-top: 4px;
+  display: flex;
+  gap: 6px;
+  align-items: center;
+  color: ${red1};
+  font-size: 13px;
+  i {
+    margin-bottom: 2px;
+  }
+`;
+
+const StarContainer = styled.div`
+  margin-top: 8px;
+  font-size: 12px;
+`;
 
 const EndPositionTag = styled.div``;
 
