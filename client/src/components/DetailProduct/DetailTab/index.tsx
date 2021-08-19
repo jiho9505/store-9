69e5c501fbf284cdf52363aea15f008063ecf926
jihoMoon <sun_days@naver.com>
2021-08-19 12:37:59 +0900

    ♻️ Refactor code  (Reflect PR reivew)

--- client/src/components/DetailProduct/DetailTab/index.tsx
+++ client/src/components/DetailProduct/DetailTab/index.tsx
@@ -11,45 +11,28 @@ import {
 import styled from '@emotion/styled';
 
 type DetailTab = {
-  index: number;
-  handleClickItemName: (e) => void;
+  choicedIdx: number;
+  handleClickItemName(e: React.MouseEvent<HTMLElement>): void;
 };
+
+const TabName = ['상품상세정보', '배송안내', '교환 및 반품안내', '상품후기', '상품문의'];
 
 /**
  * TODO:
  * TabItem의 상품 후기 , 상품 문의 카운트는
  * 상위에서 받아와야 합니다!
  */
-const DetailTab = ({ index, handleClickItemName }: DetailTab) => {
+const DetailTab = ({ choicedIdx, handleClickItemName }: DetailTab) => {
+  const createTabItem = () => {
+    return TabName.map((itemName, idx) => (
+      <TabItem key={itemName} onClick={handleClickItemName} data-idx={idx}>
+        <ItemName active={choicedIdx === idx}>{itemName}</ItemName>
+      </TabItem>
+    ));
+  };
   return (
     <TabContainer>
-      <Tab>
-        <TabItem onClick={handleClickItemName} data-idx="0">
-          <ItemName onClick={handleClickItemName} active={index === 0} data-idx="0">
-            상품상세정보
-          </ItemName>
-        </TabItem>
-        <TabItem onClick={handleClickItemName} data-idx="1">
-          <ItemName onClick={handleClickItemName} active={index === 1} data-idx="1">
-            배송안내
-          </ItemName>
-        </TabItem>
-        <TabItem onClick={handleClickItemName} data-idx="2">
-          <ItemName onClick={handleClickItemName} active={index === 2} data-idx="2">
-            교환 및 반품안내
-          </ItemName>
-        </TabItem>
-        <TabItem onClick={handleClickItemName} data-idx="3">
-          <ItemName onClick={handleClickItemName} active={index === 3} data-idx="3">
-            상품후기 <ReviewCount> 0</ReviewCount>
-          </ItemName>
-        </TabItem>
-        <TabItem onClick={handleClickItemName} data-idx="4">
-          <ItemName onClick={handleClickItemName} active={index === 4} data-idx="4">
-            상품문의 <QnaCount> 0</QnaCount>
-          </ItemName>
-        </TabItem>
-      </Tab>
+      <Tab>{createTabItem()}</Tab>
     </TabContainer>
   );
 };
