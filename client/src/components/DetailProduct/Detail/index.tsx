1aa7ff2972fda46fd7b4c338ae81d6d618c31596
jihoMoon <sun_days@naver.com>
2021-08-19 12:38:20 +0900

    ♻️ Refactor code  (Reflect PR reivew)

--- client/src/components/DetailProduct/Detail/index.tsx
+++ client/src/components/DetailProduct/Detail/index.tsx
@@ -1,5 +1,5 @@
+import React, { useRef } from 'react';
 import styled from '@emotion/styled';
-import React from 'react';
 
 import DetailTab from '../DetailTab';
 import DetailInfo from '../DetailInfo';
@@ -8,62 +8,49 @@ import ReplaceItemInfo from '../ReplaceItemInfo';
 import ProductBoard from '@/components/base/ProductBoard';
 
 const Detail = ({ info }) => {
-  const scrollIntoView = (idx) => {
-    let el;
-    switch (idx) {
-      case '0':
-        el = document.querySelector('#Tab_1');
-        el.scrollIntoView({ behavior: 'smooth' });
-        break;
-      case '1':
-        el = document.querySelector('#Tab_2');
-        el.scrollIntoView({ behavior: 'smooth' });
-        break;
-      case '2':
-        el = document.querySelector('#Tab_3');
-        el.scrollIntoView({ behavior: 'smooth' });
-        break;
-      case '3':
-        el = document.querySelector('#Tab_4');
-        el.scrollIntoView({ behavior: 'smooth' });
-        break;
-      case '4':
-        el = document.querySelector('#Tab_5');
-        el.scrollIntoView({ behavior: 'smooth' });
-        break;
-      default:
-        break;
-    }
+  const detailInfoPosition = useRef<HTMLDivElement>(null);
+  const shipInfoPosition = useRef<HTMLDivElement>(null);
+  const replaceItemInfoPosition = useRef<HTMLDivElement>(null);
+  const reviewPosition = useRef<HTMLDivElement>(null);
+  const qnaPosition = useRef<HTMLDivElement>(null);
+
+  const TabItems = [
+    <DetailInfo images={info.content_urls}></DetailInfo>,
+    <ShipInfo />,
+    <ReplaceItemInfo />,
+    <ProductBoard title="상품 후기" item={info} />,
+    <ProductBoard title="상품 문의" item={info} />,
+  ];
+
+  const refItems = [
+    detailInfoPosition,
+    shipInfoPosition,
+    replaceItemInfoPosition,
+    reviewPosition,
+    qnaPosition,
+  ];
+
+  const moveViewThroughTap = (idx: string) => {
+    const el = refItems[idx].current;
+    console.log('el: ', el);
+    el.scrollIntoView({ behavior: 'smooth' });
   };
 
-  const handleClickItemName = (e) => {
-    const idx = e.target.dataset.idx;
-    scrollIntoView(idx);
+  const handleClickItemName = (e: React.MouseEvent<HTMLElement>) => {
+    const idx = e.currentTarget.dataset.idx;
+    moveViewThroughTap(idx);
   };
-  return (
-    <DetailContainer>
-      <DetailTabContainer id="Tab_1">
-        <DetailTab index={0} handleClickItemName={handleClickItemName} />
+
+  const createInfoEachTab = () => {
+    return TabItems.map((item, idx) => (
+      <DetailTabContainer key={`${item.toString()}${idx}`} ref={refItems[idx]}>
+        <DetailTab choicedIdx={idx} handleClickItemName={handleClickItemName} />
+        {item}
       </DetailTabContainer>
-      <DetailInfo images={info.content_urls}></DetailInfo>
-      <DetailTabContainer id="Tab_2">
-        <DetailTab index={1} handleClickItemName={handleClickItemName} />
-        <ShipInfo />
-      </DetailTabContainer>
-      <DetailTabContainer id="Tab_3">
-        <DetailTab index={2} handleClickItemName={handleClickItemName} />
-        <ReplaceItemInfo />
-      </DetailTabContainer>
-      <DetailTabContainer id="Tab_4">
-        <DetailTab index={3} handleClickItemName={handleClickItemName} />
-        <ProductBoard title="상품 후기" item={info} />
-      </DetailTabContainer>
-      <DetailTabContainer id="Tab_5">
-        <DetailTab index={4} handleClickItemName={handleClickItemName} />
-        <ProductBoard title="상품 문의" item={info} />
-      </DetailTabContainer>
-    </DetailContainer>
-  );
+    ));
+  };
+
+  return <DetailContainer>{createInfoEachTab()}</DetailContainer>;
 };
 
 export default Detail;
