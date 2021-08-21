8753bb7a7d051f1b34e3c20f6405dc7d5c3cf8a8
HJ <jo920208@gmail.com>
2021-08-22 00:18:10 +0900

    ✨ Product api

---
+++ server/src/repositories/constants/product.ts
@@ -0,0 +1,11 @@
+export namespace PRODUCT_GET_MAIN {
+  export const MAIN_BEST_PRODUCT_LIMIT = 4;
+  export const MAIN_NEW_PRODUCT_LIMIT = 8;
+  export const MAIN_RECOMMEND_PRODUCT_LIMIT = 2;
+  export const MAIN_DISCOUNT_PRODUCT_LIMIT = 8;
+}
+
+export namespace PRODUCT_GET_DETAIL {
+  export const DETAIL_REVIEW_LIMIT = 5;
+  export const DETAIL_QNA_LIMIT = 5;
+}
