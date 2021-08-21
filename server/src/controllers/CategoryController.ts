e7a2367db9db1d4aad92ca40b49dd760c7b7c508
HJ <jo920208@gmail.com>
2021-08-22 07:53:58 +0900

    ⚡️ Controller export function by namespace

--- server/src/controllers/CategoryController.ts
+++ server/src/controllers/CategoryController.ts
@@ -3,21 +3,22 @@ import CategoryRepository from '../repositories/CategoryRepository';
 import CategoryResponse from '../../../shared/dtos/category/response';
 import { CategorySchema } from '../../../shared/dtos/category/schema';
 
-const getCategories: RouteHandler<null, CategoryResponse.GetCategories> = async (req, res) => {
-  const categoires = await getCustomRepository(CategoryRepository).getCategories();
+namespace CategoryController {
+  export const getCategories: RouteHandler<null, CategoryResponse.GetCategories> = async (
+    req,
+    res
+  ) => {
+    const categoires = await getCustomRepository(CategoryRepository).getCategories();
 
-  const data = categoires.map<CategorySchema>((category) => ({
-    id: category.id,
-    name: category.name,
-    level: category.level,
-    parentId: category.parent_id,
-  }));
+    const data = categoires.map<CategorySchema>((category) => ({
+      id: category.id,
+      name: category.name,
+      level: category.level,
+      parentId: category.parent_id,
+    }));
 
-  res.json({ ok: true, message: '카테고리 조회 ', data });
-};
-
-const CategoryController = {
-  getCategories,
-};
+    res.json({ ok: true, message: '카테고리 조회 ', data });
+  };
+}
 
 export default CategoryController;
--- server/src/controllers/OrderController.ts
+++ server/src/controllers/OrderController.ts
@@ -3,134 +3,132 @@ import OrderRequest from '../../../shared/dtos/order/request';
 import OrderResponse from '../../../shared/dtos/order/response';
 import OrderRepository from '../repositories/OrderRepository';
 
-const getList: RouteHandler<OrderRequest.GetList, OrderResponse.getList> = async (req, res) => {
-  try {
-    const result = await getCustomRepository(OrderRepository).getList({ userId: 1 });
+namespace OrderController {
+  export const getList: RouteHandler<OrderRequest.GetList, OrderResponse.GetList> = async (
+    req,
+    res
+  ) => {
+    try {
+      const result = await getCustomRepository(OrderRepository).getList({ userId: 1 });
 
-    res.send(result);
-  } catch (e) {
-    console.error(e);
+      res.send(result);
+    } catch (e) {
+      console.error(e);
 
-    res.status(500).json({ ok: false });
-  }
-};
+      res.status(500).json({ ok: false });
+    }
+  };
 
-const order: RouteHandler<OrderRequest.Order> = async (req, res) => {
-  try {
-    const { orderId } = req.body;
+  export const order: RouteHandler<OrderRequest.Order> = async (req, res) => {
+    try {
+      const { orderId } = req.body;
 
-    const result = await getCustomRepository(OrderRepository).order({ orderId });
+      const result = await getCustomRepository(OrderRepository).order({ orderId });
 
-    res.json({ ok: result.affected > 0 });
-  } catch (e) {
-    console.error(e);
+      res.json({ ok: result.affected > 0 });
+    } catch (e) {
+      console.error(e);
 
-    res.status(500).json({ ok: false });
-  }
-};
+      res.status(500).json({ ok: false });
+    }
+  };
 
-const cancel: RouteHandler<OrderRequest.Cancel> = async (req, res) => {
-  try {
-    const { orderId } = req.params;
+  export const cancel: RouteHandler<OrderRequest.Cancel> = async (req, res) => {
+    try {
+      const { orderId } = req.params;
 
-    const result = await getCustomRepository(OrderRepository).cancel({ orderId });
+      const result = await getCustomRepository(OrderRepository).cancel({ orderId });
 
-    res.json({ ok: result.affected > 0 });
-  } catch (e) {
-    console.error(e);
+      res.json({ ok: result.affected > 0 });
+    } catch (e) {
+      console.error(e);
 
-    res.status(500).json({ ok: false });
-  }
-};
+      res.status(500).json({ ok: false });
+    }
+  };
 
-const getCart: RouteHandler<OrderRequest.Order, OrderResponse.getCart> = async (req, res) => {
-  try {
-    const { userId = 1 } = res.locals;
+  export const getCart: RouteHandler<OrderRequest.Order, OrderResponse.GetCart> = async (
+    req,
+    res
+  ) => {
+    try {
+      const { userId = 1 } = res.locals;
 
-    const result = await getCustomRepository(OrderRepository).getCart({ userId });
+      const order = await getCustomRepository(OrderRepository).getCart({ userId });
 
-    res.json({
-      ok: true,
-      data: {
-        id: result.id,
-        status: result.status,
-        orderItems: result.items.map((orderItem) => ({
-          id: orderItem.id,
-          amount: orderItem.amount,
-          product: {
-            id: orderItem.product.id,
-            name: orderItem.product.name,
-            price: orderItem.product.price,
-            thumbnail: orderItem.product.thumbnail,
-          },
-        })),
-      },
-    });
-  } catch (e) {
-    console.error(e);
+      res.json({
+        ok: true,
+        data: {
+          id: order.id,
+          status: order.status,
+          orderItems: order.items.map((orderItem) => ({
+            id: orderItem.id,
+            amount: orderItem.amount,
+            product: {
+              id: orderItem.product.id,
+              name: orderItem.product.name,
+              price: orderItem.product.price,
+              thumbnail: orderItem.product.thumbnail,
+            },
+          })),
+        },
+      });
+    } catch (e) {
+      console.error(e);
 
-    res.status(500).json({ ok: false });
-  }
-};
+      res.status(500).json({ ok: false });
+    }
+  };
 
-const addCartItem: RouteHandler<OrderRequest.AddCartItem> = async (req, res) => {
-  try {
-    const { userId = 1 } = res.locals;
-    const { productId, amount } = req.body;
+  export const addCartItem: RouteHandler<OrderRequest.AddCartItem> = async (req, res) => {
+    try {
+      const { userId = 1 } = res.locals;
+      const { productId, amount } = req.body;
 
-    await getCustomRepository(OrderRepository).addCartItem({
-      productId,
-      amount,
-      userId,
-    });
+      await getCustomRepository(OrderRepository).addCartItem({
+        productId,
+        amount,
+        userId,
+      });
 
-    res.json({ ok: true });
-  } catch (e) {
-    console.error(e);
+      res.json({ ok: true });
+    } catch (e) {
+      console.error(e);
 
-    res.status(500).json({ ok: false });
-  }
-};
+      res.status(500).json({ ok: false });
+    }
+  };
 
-const updateCartItem: RouteHandler<OrderRequest.UpdateCartItem> = async (req, res) => {
-  try {
-    const { orderItemId, amount } = req.body;
+  export const updateCartItem: RouteHandler<OrderRequest.UpdateCartItem> = async (req, res) => {
+    try {
+      const { orderItemId, amount } = req.body;
 
-    const result = await getCustomRepository(OrderRepository).updateCartItem({
-      orderItemId,
-      amount,
-    });
+      const result = await getCustomRepository(OrderRepository).updateCartItem({
+        orderItemId,
+        amount,
+      });
 
-    res.json({ ok: result.affected > 0 });
-  } catch (e) {
-    console.error(e);
+      res.json({ ok: result.affected > 0 });
+    } catch (e) {
+      console.error(e);
 
-    res.status(500).json({ ok: false });
-  }
-};
+      res.status(500).json({ ok: false });
+    }
+  };
 
-const removeCartItem: RouteHandler<OrderRequest.RemoveCartItem> = async (req, res) => {
-  try {
-    const { orderItemId } = req.params;
+  export const removeCartItem: RouteHandler<OrderRequest.RemoveCartItem> = async (req, res) => {
+    try {
+      const { orderItemId } = req.params;
 
-    const result = await getCustomRepository(OrderRepository).removeCartItem({ orderItemId });
+      const result = await getCustomRepository(OrderRepository).removeCartItem({ orderItemId });
 
-    res.json({ ok: result.affected > 0 });
-  } catch (e) {
-    console.error(e);
+      res.json({ ok: result.affected > 0 });
+    } catch (e) {
+      console.error(e);
 
-    res.status(500).json({ ok: false });
-  }
-};
-
-const OrderController = {
-  getList,
-  order,
-  cancel,
-  getCart,
-  addCartItem,
-  updateCartItem,
-  removeCartItem,
-};
+      res.status(500).json({ ok: false });
+    }
+  };
+}
 
 export default OrderController;
