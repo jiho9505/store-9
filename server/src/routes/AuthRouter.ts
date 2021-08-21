f4ec39e6efcc1381663b9fbc02ccb7b6f5d872d4
junbeoman <wnsqja8744@gmail.com>
2021-08-21 15:00:52 +0900

    ✨ add auth routes and auth middlewares (login check, github login initial request handle function)

--- server/src/routes/AuthRouter.ts
+++ server/src/routes/AuthRouter.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
 import AuthController from '../controllers/AuthController';
-
+import AuthMiddleware from '../middlewares/auth';
 const AuthRouter = Router();
 
-AuthRouter.post('/login', AuthController.login);
-AuthRouter.post('/signup', AuthController.signup);
-AuthRouter.get('/logout', AuthController.login);
+AuthRouter.post('/login', AuthMiddleware.dataUpload, AuthController.login);
+AuthRouter.post('/signup', AuthMiddleware.dataUpload, AuthController.signup);
+AuthRouter.get('/logout', AuthMiddleware.checkLogin, AuthController.logout);
+AuthRouter.post('/github', AuthMiddleware.githubAuthInitialRequest, AuthController.githubLogin);
 
 export default AuthRouter;
