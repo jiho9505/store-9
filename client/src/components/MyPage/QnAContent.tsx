b9f5adf7a81f8e06560f8fafe6ac6a2e79886968
LeeJaeyun <ag502@naver.com>
2021-08-22 00:23:17 +0900

    ✨ qna 클릭시 포스트 모달 나타게끔 구현

--- client/src/components/MyPage/QnAContent.tsx
+++ client/src/components/MyPage/QnAContent.tsx
@@ -1,6 +1,9 @@
+import React, { useMemo, useState } from 'react';
 import { getDashFormat } from '@/utils/dateParse';
-import React, { useMemo } from 'react';
 
+import ModalPortal from '@/utils/portal';
+import PostModal from '../base/PostModal';
+import ModalWrapper from '../base/ModalWrapper';
 import ListTable from '../base/ListTable';
 import Cell from '../base/Cell';
 
@@ -14,7 +17,16 @@ const tableHeader = [
   { id: 'title', name: '제목', width: '85%' },
 ];
 
+/**
+ * 등록된 QnA를 클릭하면 해당 상품 아이디와 관련된 제품 정보를 가져온 후
+ * 작성한 내용을 불러와야 합니다.
+ * 이때 post modal이 마운트 될때 해당 정보를 네트워크 통신으로 불러올지
+ * 아니면 front-end에서 필터링을 할 지 의논해 봐야 될거 같습니다.
+ */
+
 const QnAContent = ({ questions }: QnAContentProps) => {
+  const [activeModal, setActiveModal] = useState(false);
+
   const tableBody = useMemo(() => {
     return questions.map((question) => {
       const { id, date, category, title } = question;
@@ -29,7 +41,24 @@ const QnAContent = ({ questions }: QnAContentProps) => {
     });
   }, [questions]);
 
-  return <ListTable header={tableHeader} body={tableBody} />;
+  const handleModalClose = () => {
+    setActiveModal(false);
+  };
+
+  const handleModalOpen = () => {
+    setActiveModal(true);
+  };
+
+  return (
+    <>
+      <ListTable header={tableHeader} body={tableBody} onClickRow={handleModalOpen} />
+      {activeModal && (
+        <ModalPortal>
+          <PostModal item={questions[0]} handleClickForClose={handleModalClose} title="문의하기" />
+        </ModalPortal>
+      )}
+    </>
+  );
 };
 
 export default QnAContent;
