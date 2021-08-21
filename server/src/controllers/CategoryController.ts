eb63b46c4b44e42f765d572c5e81f5f1dbf59940
HJ <jo920208@gmail.com>
2021-08-22 00:18:27 +0900

    ✨ Category api

--- server/src/controllers/CategoryController.ts
+++ server/src/controllers/CategoryController.ts
@@ -1,7 +1,23 @@
-import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import CategoryRepository from '../repositories/CategoryRepository';
+import CategoryResponse from '../../../shared/dtos/category/response';
+import { CategorySchema } from '../../../shared/dtos/category/schema';
+
+const getCategories: RouteHandler<null, CategoryResponse.GetCategories> = async (req, res) => {
+  const categoires = await getCustomRepository(CategoryRepository).getCategories();
+
+  const data = categoires.map<CategorySchema>((category) => ({
+    id: category.id,
+    name: category.name,
+    level: category.level,
+    parentId: category.parent_id,
+  }));
+
+  res.json({ ok: true, message: '카테고리 조회 ', data });
+};
 
 const CategoryController = {
-  getCategories: async (req: Request, res: Response) => {},
+  getCategories,
 };
 
 export default CategoryController;
