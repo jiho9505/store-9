daa7630336edcd686a3db3f0d9c9b17529bb682d
jihoMoon <sun_days@naver.com>
2021-08-22 22:16:50 +0900

    ✨ 스마트레이어 기능 구현

--- client/src/components/base/Header/Navigation/index.tsx
+++ client/src/components/base/Header/Navigation/index.tsx
@@ -2,23 +2,38 @@ import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import { NavLink } from '@/Router';
 import { categories, subCategories } from '@/static/constants';
-import { baeminFont, normalContainerWidth } from '@/static/style/common';
+import { baemin, baeminFont, normalContainerWidth } from '@/static/style/common';
 
 const Navigation = () => {
   const [subItemXpos, setSubItemXpos] = useState<number>(0);
   const [subItems, setSubItems] = useState([]);
+
+  let timer: number = 0;
+
   const handleMouseOverLink = (e: React.ChangeEvent<HTMLAnchorElement>) => {
-    const newSubItems = subCategories.filter(
-      (item) => item.parentName === e.currentTarget.innerText
-    );
+    const waitTime = new Promise((resolve) => {
+      timer = setTimeout(resolve, 200);
+    });
 
-    const extraXposToRemove =
-      newSubItems.length % 2
-        ? Math.floor(newSubItems.length / 2) * 100 - 25
-        : Math.floor(newSubItems.length / 2) * 100 - 85;
+    const itemName = e.currentTarget.innerText;
+    const itemXPos = e.currentTarget.getBoundingClientRect().x;
 
-    setSubItemXpos(e.currentTarget.getBoundingClientRect().x - extraXposToRemove);
-    setSubItems(newSubItems);
+    waitTime.then(() => {
+      if (!timer) return;
+      const newSubItems = subCategories.filter((item) => item.parentName === itemName);
+
+      const extraXposToRemove =
+        newSubItems.length % 2
+          ? Math.floor(newSubItems.length / 2) * 100 + 25
+          : Math.floor(newSubItems.length / 2) * 100 - 35;
+
+      setSubItemXpos(itemXPos - extraXposToRemove);
+      setSubItems(newSubItems);
+    });
+  };
+
+  const handleMouseOutLink = () => {
+    timer = 0;
   };
 
   return (
@@ -26,7 +41,11 @@ const Navigation = () => {
       <Menu>
         {categories.map(([category, path]) => (
           <Item key={category}>
-            <CategoryLink onMouseOver={handleMouseOverLink} to={path}>
+            <CategoryLink
+              onMouseOver={handleMouseOverLink}
+              onMouseOut={handleMouseOutLink}
+              to={path}
+            >
               {category}
             </CategoryLink>
           </Item>
@@ -50,13 +69,19 @@ type SubMenuProps = {
 const SubItem = styled.li`
   width: 100px;
   text-align: center;
+  cursor: pointer;
+  font-size: 13px;
+  padding: 0px 0px 15px 0px;
+  font-family: ${baeminFont};
+  &:hover {
+    color: ${baemin};
+  }
 `;
 
 const NavigationContainer = styled.div``;
 
 const SubMenu = styled.ul<SubMenuProps>`
   display: flex;
-  margin-top: 10px;
   margin-left: ${(props) => `${props.dist}px`};
 `;
 
@@ -80,4 +105,7 @@ const CategoryLink = styled(NavLink)`
   font-size: 16px;
   padding: 0 10px 0;
   font-family: ${baeminFont};
+  &:hover {
+    color: ${baemin};
+  }
 `;
