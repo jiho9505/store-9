7a0ec27d1f597b169816e2fae7a805b23047051e
junbeoman <wnsqja8744@gmail.com>
2021-08-23 14:47:44 +0900

    ✨ add review to index router

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -4,10 +4,12 @@ import AuthRouter from './AuthRouter';
 import CategoryRouter from './CategoryRouter';
 import ProductRouter from './ProductRouter';
 import UserRouter from './UserRouter';
+import ReviewRouter from './ReviewRouter';
 
 const ApiRouter = Router();
 
 ApiRouter.use('/auth', AuthRouter);
+ApiRouter.use('/reviews', ReviewRouter);
 ApiRouter.use('/users', UserRouter);
 ApiRouter.use('/products', ProductRouter);
 ApiRouter.use('/categories', CategoryRouter);
