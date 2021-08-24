c55a05fe739cfaa0d461a2dad6ddf608d164dc07
LeeJaeyun <ag502@naver.com>
2021-08-24 20:34:29 +0900

    ✨ qna 날짜 검색 기능 구현

--- client/src/pages/MyPage/QnA.tsx
+++ client/src/pages/MyPage/QnA.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { observer } from 'mobx-react';
 
 import guguStyled from '@/core/styled';
@@ -11,16 +11,8 @@ import { QnAContent } from '@/components/MyPage';
 import { getDateFormat } from '@/utils/dateParse';
 
 const QnAPage = () => {
-  const { refreshComponent } = RefreshStore;
+  const { refreshComponent, refresh } = RefreshStore;
   const [questions, setQuestions] = useState({});
-
-  useEffect(() => {
-    (async () => {
-      const questions = await QnaApi.getList();
-      setQuestions(questions.data);
-    })();
-  }, [refreshComponent]);
-
   const { form, onChange, onSetForm } = useInput({
     initialState: {
       start: '',
@@ -28,9 +20,37 @@ const QnAPage = () => {
     },
   });
 
+  useEffect(() => {
+    (async () => {
+      const questions = await getQnas(form.start, form.finish);
+      setQuestions(questions.data);
+    })();
+  }, [refreshComponent]);
+
+  const getQnas = useCallback(async (startDate, endDate) => {
+    const query: { params?: { [key: string]: string } } = {};
+    if (startDate && endDate) {
+      query.params = { startDate, endDate };
+    }
+    return await QnaApi.getList(query);
+  }, []);
+
+  const handleSubmitDate = async () => {
+    const { start, finish } = form;
+    if (!start || !finish) {
+      return;
+    }
+    refresh();
+  };
+
   return (
     <QnAPageContainer>
-      <DurationFilter form={form} onChange={onChange} onSetForm={onSetForm} />
+      <DurationFilter
+        form={form}
+        onChange={onChange}
+        onSetForm={onSetForm}
+        onSubmit={handleSubmitDate}
+      />
       <QnAContent questions={questions} />
     </QnAPageContainer>
   );
