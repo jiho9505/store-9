0343c80659de2e17dce9f7d05af4a85b7f4e67a8
jihoMoon <sun_days@naver.com>
2021-08-24 22:30:25 +0900

    ✨ Add global type

--- client/@types/global/index.d.ts
+++ client/@types/global/index.d.ts
@@ -1,3 +1,15 @@
 interface History {
   push(path: string): void;
 }
+
+type Info = {
+  discount_rate?: string;
+  title?: string;
+  price?: string;
+  image?: string;
+  content_urls?: Array<string>;
+  is_green?: boolean;
+  category_id?: string;
+  badge?: Array<string>;
+  quantity?: number;
+};
