ab1403f60efc01ff4a4673225e149f1b4cda642e
LeeJaeyun <ag502@naver.com>
2021-08-25 02:48:46 +0900

    ✨ 주문상품 날짜 쿼리 추가

--- server/src/repositories/OrderRepository.ts
+++ server/src/repositories/OrderRepository.ts
@@ -10,20 +10,28 @@ export default class OrderRepository extends Repository<Order> {
     status = OrderStatus.PURCHASING_COMPLETE,
     page = 0,
     size = 20,
+    start = new Date(0),
+    end = new Date(),
   }: {
     userId: number;
     status?: OrderStatus;
     size?: number;
     page?: number;
+    start?: Date | string;
+    end?: Date | string;
   }) {
-    console.log('a');
+    const startDate = new Date(new Date(start).setHours(0, 0, 0, 0)).toISOString().slice(0, 10);
+    const endDate = new Date(new Date(end).setHours(23, 59, 59, 59)).toISOString().slice(0, 10);
+
+    console.log(new Date(new Date(end).setHours(24, 0, 0, 0)));
+
     const result = this.query(`
       SELECT o.*, p.id as product_id, p.name, p.thumbnail, p.price, oi.amount, r.id as is_reviewed
       FROM orders o
       LEFT JOIN order_items oi ON o.id = oi.order_id
       LEFT JOIN products p ON oi.product_id = p.id
       LEFT JOIN reviews r ON r.product_id = p.id
-      WHERE o.user_id = ${userId} 
+      WHERE o.user_id = ${userId} AND DATE(o.created_at) BETWEEN '${startDate}' AND '${endDate}'
       AND o.status != '${OrderStatus.IN_CART}'
       ORDER BY updated_at DESC
       LIMIT ${size}
