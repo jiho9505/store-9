a60b28505ae2dad2e18327b9eb3064946ff61589
jihoMoon <sun_days@naver.com>
2021-08-25 18:48:49 +0900

    ♻️ Remove unused field

--- client/src/hooks/context.ts
+++ client/src/hooks/context.ts
@@ -7,13 +7,8 @@ type historyContextType = {
 
 export const HistoryContext = createContext<historyContextType>({ curLocation: '/' });
 
-type LikeModeType = 'notlogin' | 'add' | 'remove';
-
 type ProductContextType = {
   info?: Info;
-  handleClickLikeButton?: (mode: LikeModeType) => void;
-  handleClickReviewRegisterBtn?: () => void;
-  handleClickQnARegisterBtn?: () => void;
 };
 
 export const ProductContext = createContext<ProductContextType>({});
