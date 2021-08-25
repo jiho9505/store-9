d80763319b81462a240192118c67ee54e5ddee06
jihoMoon <sun_days@naver.com>
2021-08-25 18:18:20 +0900

    ✨ Like 버튼에 메세지를 결합하여 예외처리 구현 (api연동전)

--- client/src/components/DetailProduct/LikeButton/index.tsx
+++ client/src/components/DetailProduct/LikeButton/index.tsx
@@ -1,35 +1,71 @@
 import React, { useState, useContext, useEffect } from 'react';
 import styled from '@emotion/styled';
 
+import Message from '@/components/common/Message';
+
 import { greyLine } from '@/static/style/common';
 import { ProductContext } from '@/hooks/context';
+import ModalPortal from '@/utils/portal';
+import { requireLoginMsg } from '@/static/constants';
+
+type MessageModeType = 'success' | 'fail';
+type LikeModeType = 'notlogin' | 'add' | 'remove';
+const showErrorMsgTime = 1500;
 
 const Like = () => {
-  const { info, handleClickLikeButton } = useContext(ProductContext);
+  const { info } = useContext(ProductContext);
   const [isIconActive, setIsIconActive] = useState(false);
+  const [showMessage, setshowMessage] = useState<boolean>(false);
+  const [messageContent, setMessageContent] = useState<string>('');
+  const [messageMode, setMessageMode] = useState<MessageModeType>('fail');
 
-  /**
-   * TODO:
-   * 연동 시 아래와 같이 진행합니다.
-   */
+  let timer: number = 0;
+
   useEffect(() => {
     // setIsIconActive(info.userLiked)
+    clearTimeout(timer);
   }, []);
+
+  const createMsg = (mode: MessageModeType, title: string) => {
+    setshowMessage(true);
+    setMessageMode(mode);
+    setMessageContent(title);
+    timer = setTimeout(() => {
+      setshowMessage(false);
+    }, showErrorMsgTime);
+  };
+
+  const viewMsgByUserStatus = (mode: LikeModeType) => {
+    if (mode === 'notlogin') {
+      createMsg('fail', requireLoginMsg);
+    } else if (mode === 'add') {
+      createMsg('success', '관심목록에 추가하였습니다.');
+    } else if (mode === 'remove') {
+      createMsg('success', '관심목록에 제거하였습니다.');
+    }
+  };
 
   /**
    * TODO:
    * 로그인했다면 2번으로 넘어가서 post 혹은 delete하며 setState 해줍니다 (반환값 필요X)
    * 그 후 관심상품으로 등록하였다는 메시지를 보여줍니다.
    * 로그인이 안됐다면 1번에서 끝납니다.
+   *
+   * 여기서 분기문 쓰면 됩니다.
    */
   const handleClickBtn = () => {
-    handleClickLikeButton('remove');
-    setIsIconActive(!isIconActive);
+    viewMsgByUserStatus('add');
+    setIsIconActive(true);
   };
 
   return (
     <LikeContainer>
       <i className={`${isIconActive ? 'fas' : 'far'} fa-heart`} onClick={handleClickBtn}></i>
+      {showMessage && (
+        <ModalPortal>
+          <Message text={messageContent} mode={messageMode} />
+        </ModalPortal>
+      )}
     </LikeContainer>
   );
 };
