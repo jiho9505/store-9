0a91aab7f0191a2c9b950008e75fb08c8fc76d00
jihoMoon <sun_days@naver.com>
2021-08-25 20:45:41 +0900

    ✨ 장바구니 버튼에 메세지를 결합하여 예외처리 구현 (api연동전)

--- client/src/components/DetailProduct/CartButton/index.tsx
+++ client/src/components/DetailProduct/CartButton/index.tsx
@@ -1,22 +1,65 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import styled from '@emotion/styled';
+
+import Message from '@/components/common/Message';
 
 import { baeminFont, greyLine } from '@/static/style/common';
 import { ProductContext } from '@/hooks/context';
+import ModalPortal from '@/utils/portal';
+import { requireLoginMsg, showErrorMsgTime } from '@/static/constants';
+
+type MessageModeType = 'success' | 'fail';
+type CartModeType = 'notlogin' | 'add';
+const addCartMsg = '장바구니에 추가하였습니다.';
 
 const Cart = () => {
   const { info } = useContext(ProductContext);
+  const [showMessage, setshowMessage] = useState<boolean>(false);
+  const [messageContent, setMessageContent] = useState<string>('');
+  const [messageMode, setMessageMode] = useState<MessageModeType>('fail');
+
+  let timer: number = 0;
+
+  useEffect(() => {
+    return () => clearTimeout(timer);
+  }, []);
+
   /**
    * TODO:
+   * 로그인 유무에 따라
    * POST (userid,productid,stock ...etc)
+   * 에러메시지 띄웁니다
    */
   const handleClickText = () => {
-    history.push('/cart');
+    viewMsgByUserStatus('add');
+    // history.push('/cart');
+  };
+
+  const createMsg = (mode: MessageModeType, title: string) => {
+    setshowMessage(true);
+    setMessageMode(mode);
+    setMessageContent(title);
+    timer = setTimeout(() => {
+      setshowMessage(false);
+    }, showErrorMsgTime);
+  };
+
+  const viewMsgByUserStatus = (mode: CartModeType) => {
+    if (mode === 'notlogin') {
+      createMsg('fail', requireLoginMsg);
+    } else if (mode === 'add') {
+      createMsg('success', addCartMsg);
+    }
   };
 
   return (
     <CartContainer onClick={handleClickText}>
       <span>장바구니</span>
+      {showMessage && (
+        <ModalPortal>
+          <Message text={messageContent} mode={messageMode} />
+        </ModalPortal>
+      )}
     </CartContainer>
   );
 };
