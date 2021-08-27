c25d9f7167a508255278bc5cbf65c13b3de20187
jihoMoon <sun_days@naver.com>
2021-08-27 22:54:07 +0900

    ✨ API연동 및 Logic Fe->Be로 이동

--- client/src/pages/Main/index.tsx
+++ client/src/pages/Main/index.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import styled from '@emotion/styled';
 
-import { getMainList } from '@/remotes/ProductRemote';
 import Carousel from '@/components/Main/Carousel/Carousel';
 import FeatureSection from '@/components/Main/FeatureSection/FeatureSection';
 import GiftSection from '@/components/Main/GiftSection/GiftSection';
-import Datas from '@/dummy';
+import ProductApi from '@/apis/ProductApi';
+
+const alertMsg = '아이템을 불러오는데 실패하였습니다.';
 
 const Main = () => {
   const [products, setProducts] = useState({
@@ -14,54 +15,22 @@ const Main = () => {
     discount: [],
   });
 
-  /**
-   * useEffect에는 하위 주석만 있으면 됩니다.
-   */
   useEffect(() => {
-    // (async () => {
-    //   const data = await getMainList();
-    //   setProducts({
-    //     best: data.bestProducts,
-    //     new: data.newProducts,
-    //     discount: data.discountProducts,
-    //   });
-    // })();
-
-    let bestProducts = [];
-    let newProducts = [];
-    let discountProducts = [];
-
-    Datas.filter((data) => data.stock).forEach((data) => {
-      if (data.badges.includes('best')) bestProducts.push(data);
-      if (data.badges.includes('new')) newProducts.push(data);
-      if (data.badges.includes('sale')) discountProducts.push(data);
-    });
-
-    bestProducts = extractRandomlyProduct(bestProducts, 4);
-    newProducts = extractRandomlyProduct(newProducts, 8);
-    discountProducts = extractRandomlyProduct(discountProducts, 8);
-
-    setProducts({
-      best: bestProducts,
-      new: newProducts,
-      discount: discountProducts,
-    });
+    (async () => {
+      try {
+        const result = await ProductApi.getMain();
+        result.data.bestProducts[0].stock = 1;
+        result.data.bestProducts[0].discountRate = 1;
+        setProducts({
+          best: result.data.bestProducts,
+          new: result.data.newProducts,
+          discount: result.data.discountProducts,
+        });
+      } catch (e) {
+        alert(alertMsg);
+      }
+    })();
   }, []);
-
-  /**
-   * best, new , sale 각각 진열될 갯수보다 많기때문에
-   * 매번 랜덤하게 다른 상품들을 보여준다.
-   * 다만 최소 진열될 갯수만큼의 아이템이 필요하다!
-   */
-  const extractRandomlyProduct = (productArray, extractNum) => {
-    const randomItems = [];
-    const totalLength = productArray.length;
-    while (productArray.length > totalLength - extractNum) {
-      let target = productArray.splice(Math.floor(Math.random() * productArray.length), 1)[0];
-      randomItems.push(target);
-    }
-    return randomItems;
-  };
 
   return (
     <MainContainer>
