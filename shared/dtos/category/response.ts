04824fb8c9b20eb2c8e53ab452cdf365d3a4cfce
junbeoman <wnsqja8744@gmail.com>
2021-08-28 17:29:12 +0900

    🏷️ change auth type

--- shared/dtos/category/response.ts
+++ shared/dtos/category/response.ts
@@ -1,19 +1,16 @@
 namespace CategoryResponse {
   export type GetCategories = {
-    id: number;
-    name: string;
-    parentId: number;
-    // parentCategories: {
-    //   id: number;
-    //   name: string;
-    //   parentId: null;
-    // }[];
-    // subCategories: {
-    //   id: number;
-    //   name: string;
-    //   parentId: number;
-    // }[];
-  }[];
+    parentCategories: {
+      id: number;
+      name: string;
+      parentId: null;
+    }[];
+    subCategories: {
+      id: number;
+      name: string;
+      parentId: number;
+    }[];
+  };
 }
 
 export default CategoryResponse;
