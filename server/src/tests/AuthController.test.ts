13096c0236e95f7ca7fb4d51a309ba934c85f83c
junbeoman <wnsqja8744@gmail.com>
2021-08-28 17:40:54 +0900

    ✅ simple server test

--- server/src/tests/AuthController.test.ts
+++ server/src/tests/AuthController.test.ts
@@ -2,6 +2,7 @@ import constant from '../utils/constant';
 import request from 'supertest';
 import { app } from '../../app';
 
+// given
 const server = request.agent(app);
 const AUTH_ROOT = '/api/auth';
 const SAMPLE_ID = 'user123';
@@ -9,29 +10,35 @@ const SAMPLE_PWD = '123456';
 const WRONG_PWD = '1234';
 
 describe('Auth test', () => {
-  describe('login', () => {
-    it('auth check without login', async () => {
+  describe('로그인 테스트', () => {
+    it('로그인 전 로그인 체크, 실패 사인 false를 받아야한다.', async () => {
+      // when
       const res = await server.get(`${AUTH_ROOT}/check`).expect(403);
 
+      // then
       expect(res.ok).toBe(false);
     });
 
-    it('wrong login', async () => {
+    it('로그인 정보 틀린후, 로그인 체크시 실패 사인 false와 함께 로그인이 필요하다는 메시지가 필요하다.', async () => {
+      // when
       const loginRes = await server
         .post(`${AUTH_ROOT}/login`)
         .send({ id: SAMPLE_ID, password: WRONG_PWD })
         .expect(constant.STATUS_AUTH_FAILURE);
 
+      // then
       expect(loginRes.body.ok).toBe(false);
       expect(loginRes.body.message).toBe(constant.WRONG_INFO);
 
+      // when
       const checkRes = await server.get(`${AUTH_ROOT}/check`).expect(constant.STATUS_NO_AUTHORIZED);
 
+      // then
       expect(checkRes.body.ok).toBe(false);
       expect(checkRes.body.message).toBe(constant.LOGIN_REQUIRED);
     });
 
-    it('auth check after proper login', async () => {
+    it('로그인 올바르게 된 후 ', async () => {
       const loginRes = await server
         .post(`${AUTH_ROOT}/login`)
         .send({ id: SAMPLE_ID, password: SAMPLE_PWD })
