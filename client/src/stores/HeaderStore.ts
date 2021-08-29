4b6b2cfac98cba0c23809bf1a8bd0b07be2eef7f
jihoMoon <sun_days@naver.com>
2021-08-29 22:27:05 +0900

    ✨ Add header store

---
+++ client/src/stores/HeaderStore.ts
@@ -0,0 +1,33 @@
+import CategoryApi from '@/apis/CategoryApi';
+import { makeAutoObservable } from 'mobx';
+
+type CategoryType = {
+  id: number;
+  name: string;
+  parentId: number | null;
+}[];
+
+class CaregoryStore {
+  parentCategories: CategoryType;
+  subCategories: CategoryType;
+  productNames: string[];
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  async load() {
+    try {
+      const result = await CategoryApi.getCategories();
+      if (result.ok) {
+        this.parentCategories = result.data.parentCategories;
+        this.subCategories = result.data.subCategories;
+        this.productNames = result.data.productNames;
+      }
+    } catch (e) {
+      alert(e.response.data.message);
+    }
+  }
+}
+
+export default new CaregoryStore();
