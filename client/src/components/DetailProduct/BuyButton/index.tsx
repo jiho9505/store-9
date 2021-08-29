0dcdb6e54938d319768eb6baa3ee9919586ec9f3
jihoMoon <sun_days@naver.com>
2021-08-29 19:58:11 +0900

    ♻️ Refactor from context to mobx

--- client/src/components/DetailProduct/BuyButton/index.tsx
+++ client/src/components/DetailProduct/BuyButton/index.tsx
@@ -1,47 +1,53 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
 
 import Message from '@/components/common/Message';
 
 import useHistory from '@/hooks/customHooks/useHistory';
 import useLocalStorage from '@/hooks/customHooks/useLocalStorage';
-import { ProductContext } from '@/hooks/context';
 import { greyLine, baeminFont } from '@/static/style/common';
 import AuthStore from '@/stores/AuthStore';
 import ModalPortal from '@/utils/portal';
 import { showErrorMsgTime } from '@/static/constants';
 import { alertMsg } from '@/utils/errorMessage';
 import OrderApi from '@/apis/OrderApi';
+import DetailProductStore from '@/stores/DetailProductStore';
 
 const errorMsg = '과정에서 에러가 발생하였습니다';
 
 type BuyProps = {
   selectedStock: number;
 };
+let timer: number = 0;
 
 const Buy = ({ selectedStock }: BuyProps) => {
-  const { info } = useContext(ProductContext);
   const history = useHistory();
   const [BuyInfo, setBuyInfo] = useLocalStorage('cartInfo', {});
   const [showMessage, setShowMessage] = useState<boolean>(false);
-
-  let timer: number = 0;
 
   useEffect(() => {
     return () => clearTimeout(timer);
   }, []);
 
-  /**
-   * TODO:
-   * 반환값을 이용하여
-   * 슬랙에서 받은 사진과 같은 데이터 형식으로 보낼것
-   */
   const handleClickText = async () => {
     if (!AuthStore.isLogined) return createMsg();
     try {
-      const result = await OrderApi.addCartItem({ productId: 55, amount: selectedStock });
+      const result = await OrderApi.addCartItem({
+        productId: DetailProductStore.product.productId,
+        amount: selectedStock,
+      });
       if (result.ok) {
-        setBuyInfo(info);
+        const { id, amount, orderId } = result.data;
+        setBuyInfo({
+          cartId: id,
+          products: [
+            {
+              id: orderId,
+              amount,
+              product: DetailProductStore.product,
+            },
+          ],
+        });
         history.push('/order');
       }
     } catch (e) {
