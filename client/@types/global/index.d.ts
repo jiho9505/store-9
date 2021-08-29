531d5310dbce443074b410a1485064f33c4f6621
jihoMoon <sun_days@naver.com>
2021-08-29 18:45:59 +0900

    ✨ Remove unused Type

--- client/@types/global/index.d.ts
+++ client/@types/global/index.d.ts
@@ -2,21 +2,6 @@ interface History {
   push(path: string): void;
 }
 
-type Info = {
-  discountRate?: number;
-  name?: string;
-  price?: number;
-  thumbnail?: string;
-  content_urls?: Array<string>;
-  isGreen?: boolean;
-  category_id?: string;
-  badges?: Array<string>;
-  stock?: number;
-  reviewAverageRate?: number;
-  reviewCount?: number;
-  likeCount?: number;
-};
-
 type MessageModeType = 'success' | 'fail' | 'caution';
 
 type Message = {
